e932533e1ec10a730e7b1101a4ed8e4a5f3438bb
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-02-18 16:19:39 +0100

    feature(layout): setup authentication pages and navigation

---
+++ src/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-page padding class="text-center">
+    <h1 class="text-h4 text-grey-8">Bienvenue sur Island Crossing !</h1>
+    <div class="q-gutter-lg">
+      <q-btn color="primary" label="Connexion" to="login" />
+      <q-btn color="secondary" label="Inscription" to="register" />
+    </div>
+  </q-page>
+</template>
