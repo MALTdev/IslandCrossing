f3ed561d9c1e7cf00637f8271c2239e99417ba95
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-02-18 16:18:25 +0100

    feature(layout): create layout with header and footer

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-footer class="bg-secondary text-center">
+    <q-toolbar class>
+      <q-toolbar-title>
+        <q-avatar>
+          <img
+            src="https://cdn.pixabay.com/photo/2020/07/27/10/33/animal-crossing-leaf-5442035_1280.png"
+          />
+        </q-avatar>
+        <div class="text-subtitle1">© 2022 - Island Crossing - Tous droits réservés</div>
+      </q-toolbar-title>
+    </q-toolbar>
+  </q-footer>
+</template>
