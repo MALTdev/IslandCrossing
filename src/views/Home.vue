b7c255c8b24b7d4cc9edfa63ad1d756bc775f082
LoganLS <logan_56@hotmail.fr>
2022-03-07 17:52:25 +0100

    section happy birthday + fix css on other sections

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -36,15 +36,31 @@
           <div
             class="item-menu-rounded"
           >
-            <q-img :src="'src/assets/images/menu/' + item.icon"/>
+            <q-img :src="'src/assets/images/menu/' + item.icon" fit="contain"/>
           </div>
           <span class="item-menu-text">{{ item.text }}</span>
         </router-link>
       </div>
     </section>
 
-    <section id="section-happy-birthday">
+    <section id="section-happy-birthday" v-if="birthdays">
       <h2>Les anniversaires des habitants, le {{ dateNow }}</h2>
+      <div id="happy-birthday-villager">
+          <div id="happy-birthday-villager-card-and-icons">
+            <q-card id="happy-birthday-villager-card">{{ birthdays[birthdaySelected].name }} fête son anniversaire aujourd'hui !</q-card>
+            <div id="happy-birthday-village-change-villager" v-if="birthdays.length > 1">
+              <q-btn
+                icon="fas fa-carret-left"
+                @click="getPreviousBirthday()"
+              />
+              <q-btn
+                icon="fas fa-carret-right"
+                @click="getNextBirthday()"
+              />
+            </div>
+          </div>
+          <q-img id="happy-birthday-villager-image" :src="birthdays[birthdaySelected].url" fit="contain"/>
+      </div>
     </section>
 
     <section id="section-last-publications">
@@ -130,6 +146,42 @@ const publications = ref<Array<Object>>([
     title: 'Title 3',
   }
 ])
+
+const birthdaySelected = ref<number>(0)
+
+const birthdays = ref<Array<Object>>([
+  {
+    url: 'http://ekladata.com/0sERZpktjWhwS4xcIg8nk3MmiH8.png',
+    name: 'George',
+  },
+  {
+    url: 'https://animalcrossingwiki.de/_media/nachbarn/daniel/daniel_nl.png',
+    name: 'Daniel',
+  },
+  {
+    url: 'https://www.pngkit.com/png/full/243-2438030_new-leaf-who-is-your-favorite-rabbit-villager.png',
+    name: 'Carmen',
+  }
+])
+
+function getPreviousBirthday() {
+  if (this.birthdaySelected !== 0) {
+      this.birthdaySelected--
+      return
+  }
+
+  this.birthdaySelected = this.birthdays.length - 1
+}
+
+function getNextBirthday() {
+  if (this.birthdaySelected === this.birthdays.length - 1) {
+      this.birthdaySelected = 0
+      return
+  }
+
+  this.birthdaySelected++
+}
+
 </script>
 
 <style>
