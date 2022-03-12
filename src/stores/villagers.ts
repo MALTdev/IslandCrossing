68968fca4145ba33ab3e9dcadc5c0dee5987872e
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-03-12 12:16:14 +0100

    fix(store): cleaning type and call in villagers store

--- src/stores/villagers.ts
+++ src/stores/villagers.ts
@@ -1,39 +1,36 @@
-import { ref } from "vue";
 import { defineStore } from "pinia";
-import { useLocalStorage } from "@vueuse/core";
 import http from "@/plugins/axios";
-import { useUserStore } from "@/stores/user"
-import type { User } from "@/stores/user"
+
+import { useUserStore } from "@/stores/user";
 
 export interface Villager {
-	id?: number;
-	birthday_day?: number;
-	birthday_montyh?: string;
-	catchphrase?: string;
-	clothing?: string;
-  	code?: string;
-	created_at?: string;
-	gender_id?: number;
-	icon_url?: string;
-	image_url?: string;
-	lang_id?: number;
-	name?: string;
-	personality_id?: number;
-	phrase?: string;
-	sign_id?: number;
-	species_id?: number;
-	updated_at?: string;
-	url?: string;
+  id: number;
+  birthday_day?: number;
+  birthday_montyh?: string;
+  catchphrase?: string;
+  clothing?: string;
+  code?: string;
+  created_at?: string;
+  gender_id?: number;
+  icon_url?: string;
+  image_url?: string;
+  lang_id?: number;
+  name?: string;
+  personality_id?: number;
+  phrase?: string;
+  sign_id?: number;
+  species_id?: number;
+  updated_at?: string;
+  url?: string;
 }
 
 export const useVillagersStore = defineStore("villagersStore", () => {
-  const {getToken} = useUserStore()
+  const { getToken } = useUserStore();
 
-  function getVillagers() {
+  function getVillagers(): Promise<Villager[]> {
     return new Promise(async (resolve, reject) => {
-      const villagers = await (
-        await http.get(`/api/villagers?api_token=${getToken}`)
-      ).data;
+      const villagers = (await http.get(`/api/villagers?api_token=${getToken}`))
+        .data;
 
       return resolve(villagers);
     });
@@ -41,7 +38,7 @@ export const useVillagersStore = defineStore("villagersStore", () => {
 
   function getNamesAndCodeVillagers() {
     return new Promise(async (resolve, reject) => {
-      const namesVillagers = await (
+      const namesVillagers = (
         await http.get(`/api/villagers/getnames?api_token=${getToken}`)
       ).data;
 
@@ -49,20 +46,19 @@ export const useVillagersStore = defineStore("villagersStore", () => {
     });
   }
 
-  //function getVillagersFiltered(param: string, value: string) {
-  function getVillagersFiltered(params: string) {
-	return new Promise(async (resolve, reject) => {
-		const villagers = await (
-		  await http.get(`/api/villagers/search?api_token=${getToken}${params}`)
-		).data;
-  
-		return resolve(villagers);
-	  });
+  function getVillagersFiltered(params: string): Promise<Villager[]> {
+    return new Promise(async (resolve, reject) => {
+      const villagers = (
+        await http.get(`/api/villagers/search?api_token=${getToken}${params}`)
+      ).data;
+
+      return resolve(villagers);
+    });
   }
 
-  function getVillager(id: number) {
+  function getVillager(id: number): Promise<Villager> {
     return new Promise(async (resolve, reject) => {
-      const villager = await (
+      const villager = (
         await http.get(`/api/villagers/${id}?api_token=${getToken}`)
       ).data;
 
@@ -70,9 +66,9 @@ export const useVillagersStore = defineStore("villagersStore", () => {
     });
   }
 
-  function getVillagersBirthdays() {
+  function getVillagersBirthdays(): Promise<Villager[]> {
     return new Promise(async (resolve, reject) => {
-      const villager = await (
+      const villager = (
         await http.get(`/api/villagers-anniversary-today?api_token=${getToken}`)
       ).data;
 
@@ -80,5 +76,11 @@ export const useVillagersStore = defineStore("villagersStore", () => {
     });
   }
 
-  return { getVillagers, getNamesAndCodeVillagers, getVillagersFiltered, getVillager,getVillagersBirthdays };
+  return {
+    getVillagers,
+    getNamesAndCodeVillagers,
+    getVillagersFiltered,
+    getVillager,
+    getVillagersBirthdays,
+  };
 });
