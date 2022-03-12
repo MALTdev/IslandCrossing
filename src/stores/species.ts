656dfa1fa37271231ef6dd2f5e99bbe44de31f79
AnaelBonnafous <anael.bonnafous@gmail.com>
2022-03-12 15:28:23 +0100

    fix(store): cleaning type and call in species store

--- src/stores/species.ts
+++ src/stores/species.ts
@@ -1,32 +1,29 @@
-import { ref } from "vue";
 import { defineStore } from "pinia";
-import { useLocalStorage } from "@vueuse/core";
 import http from "@/plugins/axios";
-import { useUserStore } from "@/stores/user"
-import type { User } from "@/stores/user"
+
+import { useUserStore } from "@/stores/user";
 
 export interface Specie {
-	id?: number;
+  id?: number;
   code?: string;
   name?: string;
 }
 
 export const useSpeciesStore = defineStore("speciesStore", () => {
-  const {getToken} = useUserStore()
+  const { getToken } = useUserStore();
 
-  function getSpecies() {
+  function getSpecies(): Promise<Specie[]> {
     return new Promise(async (resolve, reject) => {
-      const species = await (
-        await http.get(`/api/species?api_token=${getToken}`)
-      ).data;
+      const species = (await http.get(`/api/species?api_token=${getToken}`))
+        .data;
 
       return resolve(species);
     });
   }
 
-  function getSpeciesNames() {
+  function getSpeciesNames(): Promise<Specie[]> {
     return new Promise(async (resolve, reject) => {
-      const species = await (
+      const species = (
         await http.get(`/api/species/getnames?api_token=${getToken}`)
       ).data;
 
